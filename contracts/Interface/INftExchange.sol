42056a4eebfae50f33fb190b43954c63aab01176
Kyle Longrich <klongrich@kyles-mbp.home>
2021-05-27 17:21:52 -0400

    updating interface

--- contracts/Interface/INftExchange.sol
+++ contracts/Interface/INftExchange.sol
@@ -10,7 +10,7 @@ interface INftExchange {
         @param BuyerID ID of the NFT being preposed to the lister.
         @param ListerContract Address of the NFT that is Listed or Being asked for.
         @param ListerID ID of the NFT that is Listed or Being asked for.
-        @param OrderID Unqiue number that will save the order on chain.
+        @param orderID Unqiue number that will save the order on chain.
         
         Emits: 
     */
@@ -19,9 +19,8 @@ interface INftExchange {
         address BuyerContract,
         uint256 BuyerID,
         address ListerContract,
-        uint256 ListerID,
-        uint256 OrderID
-    ) external returns (bool);
+        uint256 ListerID
+    ) external returns (bytes32 orderID);
 
     /**
      * @dev Creates an Offer of One NFT for a static amount of ERC20 tokens  
@@ -29,7 +28,7 @@ interface INftExchange {
         @param ListerID ID of the NFT that is Listed or Being asked for.
         @param Erc20Contract Address of the ERC20 Token that is being offered
         @param Amount The total number of ERC20 tokens being offered
-        @param OrderID Unqiue number that will save the order on chain.
+        @param orderID Unqiue number that will save the order on chain.
         
         Emits: 
     */
@@ -38,34 +37,32 @@ interface INftExchange {
         address ListerContract,
         uint256 ListerID,
         address Erc20Contract,
-        uint256 Amount,
-        uint256 OrderID
-    ) external payable returns (bool);
+        uint256 Amount
+    ) external payable returns (bytes32 orderID);
 
     /**
      * @dev Creates an Offer of One NFT for a static amount of ERC20 tokens  
         @param ListerContract Address of the NFT that is Listed or Being asked for.
         @param ListerID ID of the NFT that is Listed or Being asked for.
-        @param OrderID Unqiue number that will save the order on chain.
+        @param orderID Unqiue number that will save the order on chain.
         
         Emits: 
     */
 
     function makeNftToETH(
         address ListerContract,
-        uint256 ListerID,
-        uint256 OrderID
-    ) external payable returns (bool);
+        uint256 ListerID
+    ) external payable returns (bytes32 orderID);
 
     /**
      * @dev Triggers the trasnfer of assests for between the two parties once aggreed
         upon terms are matched. This is only callable by the person who owns the NFT
         that is being asked for.
         @param Buyer Address of the person submiting the offer
-        @param OrderID Unqiue ID that is given the the order stored on chain
+        @param orderID Unqiue ID that is given the the order stored on chain
         
         Emits: 
     */
 
-    function takeOrder(address Buyer, address payable Reciver, uint256 OrderID) external returns (bool);
+    function takeOrder(address Buyer, address payable Reciver, bytes32 orderID) external returns (bool);
 }
